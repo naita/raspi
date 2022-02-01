33ae2226278ae52a1e98069da978c9099ef0ffd2
naita <provence4@gmail.com>
2022-02-01 09:14:53 +0100

    1.0 take photo every hour

---
+++ capture_image.py
@@ -0,0 +1,56 @@
+from picamera import PiCamera
+import time
+from datetime import datetime
+
+
+
+# take foto
+def captureImage(fname):
+    camera = PiCamera()
+    camera.resolution = (1024,768)
+    camera.framerate = 30
+    camera.vflip = True
+
+    now = datetime.now()
+    dt = now.strftime("%Y%m%d_%H:%M:%S")
+    print(dt)
+    try:
+        camera.capture(fname+dt+".jpg") # ,use_video_port=True)
+        time.sleep(5)
+    finally:
+        camera.close() #sleep(10)
+
+def captureVideo():
+    camera = PiCamera()
+    camera.resolution = (1024,768)
+    camera.framerate = 30
+    camera.vflip = True
+
+    now = datetime.now()
+    dt = now.strftime("%Y%m%d_%H%M%S")
+    camera.start_recording("filmpje_"+str(dt)+".h264")
+    time.sleep(15)
+    camera.stop_recording()
+    camera.close()
+
+
+
+while True:
+    captureImage("kweekkas_")
+    time.sleep(60*60)
+    #captureVideo()
+    #time.sleep(1*5)
+
+''''
+while True:
+    now = datetime.now()
+    dt = now.strftime("%Y%m%d_%H%M%S")
+    pir.wait_for_motion()
+    print("motion detected")
+    camera.start_recording("filmpje_"+str(dt)+".h264")
+    time.sleep(10)
+    pir.wait_for_no_motion()
+    print("no motion")
+#    camera.stop_recording()
+
+'''
